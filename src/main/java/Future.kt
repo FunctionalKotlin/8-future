e82ec9f36b5cd50b44d161724ca8e7eb8229e555
Alejandro Hernández <info@alejandrohdezma.com>
2017-11-19 23:59:47 +0100

    The Applying King

--- src/main/java/Future.kt
+++ src/main/java/Future.kt
@@ -37,6 +37,23 @@ fun <A, B> Future<A>.flatMap(transform: (A) -> Future<B>): Future<B> =
         transform(this@flatMap.task.await()).task.await()
     })
 
+fun <A, B> Future<A>.apply(futureAB: Future<(A) -> B>): Future<B> =
+    Future(async(CommonPool) {
+        val a = this@apply.task.await()
+        val ab = futureAB.task.await()
+
+        ab(a)
+    })
+
+infix fun <A, B> ((A) -> B).map(future: Future<A>): Future<B> =
+    future.map(this)
+
+infix fun <A, B> Future<(A) -> B>.ap(future: Future<A>): Future<B> =
+    future.apply(this)
+
+infix fun <A, B> Future<A>.bind(transform: (A) -> Future<B>): Future<B> =
+    flatMap(transform)
+
 fun main(args: Array<String>) {
     asyncFuture { 23 + 19 }
         .map { it + 3 }
