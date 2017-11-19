7ea2c239f4a2d48eaa63de2d998cfb5a71c1a777
Alejandro Hernández <info@alejandrohdezma.com>
2017-11-19 23:23:45 +0100

    Task Force X

--- src/main/java/Future.kt
+++ src/main/java/Future.kt
@@ -1,4 +1,18 @@
 // Copyright © FunctionalKotlin.com 2017. All rights reserved.
 
-class Future {
+class Future(val task: () -> Unit) {
+}
+
+fun main(args: Array<String>) {
+    val getNumber = {
+        23 + 19
+    }
+
+    val task = {
+        getNumber()
+
+        Unit
+    }
+
+    val future = Future(task)
 }
