cb11ea82a185eae87ae2d98df22b4664e0edf212
Alejandro Hernández <info@alejandrohdezma.com>
2017-11-19 23:59:52 +0100

    Top Five

--- src/main/java/App.kt
+++ src/main/java/App.kt
@@ -9,7 +9,13 @@ fun parseURL(url: String): JsonObject = URL(url)
 
 data class Post(val title: String, val content: String, val authorId: Int)
 
-data class Author(val firstName: String, val lastName: String)
+val Post.wordCount: Int
+    get() = content.split(" ").size
+
+data class Author(val firstName: String, val lastName: String, val lastPostId: Int)
+
+val Author.lastPost: Future<Post>
+    get() = getPost(lastPostId)
 
 fun JsonObject.toPost(): Post {
     val title = this["title"] as String
@@ -22,8 +28,9 @@ fun JsonObject.toPost(): Post {
 fun JsonObject.toAuthor(): Author {
     val firstName = this["firstName"] as String
     val lastName = this["lastName"] as String
+    val lastPostId = this["lastPost"] as Int
 
-    return Author(firstName, lastName)
+    return Author(firstName, lastName, lastPostId)
 }
 
 fun getPost(id: Int): Future<Post> = asyncFuture {
@@ -34,7 +41,20 @@ fun getAuthor(id: Int): Future<Author> = asyncFuture {
     parseURL("http://functionalhub.com/exercises/users/$id").toAuthor()
 }
 
+fun topFive(): Future<List<Int>> = asyncFuture {
+    parseURL("http://functionalhub.com/exercises/top-users")
+        .map { it.value as String }
+        .map(String::toInt)
+}
+
+fun average(first: Int, second: Int, third: Int, fourth: Int, fifth: Int): Int =
+    (first + second + third + fourth + fifth) / 5
+
 fun main(args: Array<String>) {
+    val lastPostWordCount: (Int) -> Future<Int> = {
+        getAuthor(it).flatMap { it.lastPost }.map { it.wordCount }
+    }
+
     getPost(1)
         .flatMap { post ->
             getAuthor(post.authorId)
@@ -42,6 +62,17 @@ fun main(args: Array<String>) {
         .runAsync { user ->
             println(user) // User(firstname: "Megan", lastName: "Sanchez")
         }
+
+    topFive().flatMap { topFive ->
+        ::average.curried() map
+            lastPostWordCount(topFive[0]) ap
+            lastPostWordCount(topFive[1]) ap
+            lastPostWordCount(topFive[2]) ap
+            lastPostWordCount(topFive[3]) ap
+            lastPostWordCount(topFive[4])
+    }.runAsync {
+        println(it)
+    }
 
     Thread.sleep(2000)
 }
