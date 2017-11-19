aedf095c0fc83801bf8f459be2c1771aedd6c1bd
Alejandro Hernández <info@alejandrohdezma.com>
2017-11-20 00:06:24 +0100

    Invalidation

--- src/main/java/Validators.kt
+++ src/main/java/Validators.kt
@@ -1,26 +1,31 @@
 // Copyright © FunctionalKotlin.com 2017. All rights reserved.
 
-typealias Validator<T, E> = (T) -> Result<T, E>
+typealias Validator<A, E> = (A) -> Future<Result<A, E>>
 
-infix operator fun <T, E> (Validator<T, E>)
-    .plus(validator: Validator<T, E>): Validator<T, E> = {
-        this(it).flatMap(validator)
+infix operator fun <T, E> (Validator<T, E>).plus(
+    validator: Validator<T, E>): Validator<T, E> = {
+        this(it).flatMap {
+            when(it) {
+                is Success -> validator(it.value)
+                is Failure -> Future.pure(it)
+            }
+        }
     }
 
-infix fun <A, E> (Validator<A, E>)
-    .or(validator: Validator<A, E>): Validator<A, E> = {
-        val result = this(it)
-
-        when(result) {
-            is Success -> result
-            is Failure -> validator(it)
+infix fun <A, E> (Validator<A, E>).or(
+    validator: Validator<A, E>): Validator<A, E> = { a ->
+        this(a).flatMap {
+            when(it) {
+                is Failure -> validator(a)
+                is Success -> Future.pure(it)
+            }
         }
     }
 
 fun <A> validate(with: (A) -> Boolean): (A) -> A? = { it.takeIf(with) }
 
 infix fun <A, E> ((A) -> A?).orElseFail(with: E): Validator<A, E> = { a ->
-    this(a)?.let(::Success) ?: Failure(with)
+    asyncFuture { this(a)?.let(::Success) ?: Failure(with) }
 }
 
 fun <A, E> allOf(vararg validators: Validator<A, E>): Validator<A, E> =
