22e19f0babbd4f5bfc2dee1496c27ef9d0698593
Alejandro Hernández <info@alejandrohdezma.com>
2017-11-19 23:26:29 +0100

    Back to the Future Part III

--- src/main/java/Future.kt
+++ src/main/java/Future.kt
@@ -1,6 +1,21 @@
 // Copyright © FunctionalKotlin.com 2017. All rights reserved.
 
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.launch
+
+
 class Future<A>(val task: ((A) -> Unit) -> Unit) {
+    companion object {
+        fun <A> async(getValue: () -> A): Future<A> {
+            val task: ((A) -> Unit) -> Unit = { continuation ->
+                launch(CommonPool) {
+                    continuation(getValue())
+                }
+            }
+
+            return Future(task)
+        }
+    }
 }
 
 fun main(args: Array<String>) {
